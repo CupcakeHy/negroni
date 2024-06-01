2e65b1865872295843ea48f5868546f99b7152a3
CupcakeHy <cupcakehy@gmail.com>
2024-06-01 12:29:06 +0200

    Moved cronjob

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,9 @@
-import { Client, GatewayIntentBits, Options } from 'discord.js';
+import { Client, GatewayIntentBits, Options, TextChannel, User } from 'discord.js';
 import Events from './events/index.js';
 import Keys from './keys.js';
-import {
-	registerEvents,
-} from './utils/index.js';
+import { registerEvents } from './utils/index.js';
+import { CronJob } from 'cron';
+import fs from 'fs';
 
 const client = new Client({
 	intents: [
@@ -21,3 +21,22 @@ client.login(Keys.token)
 		console.error("[ERROR]:", err);
 		process.exit(1);
 	});
+
+var dateMonth = new Date().getMonth() + 1;
+var dateDay = new Date().getDate();
+
+const job = CronJob.from({
+	cronTime: `0 0 0 * * *`,
+	onTick: async function () {
+		var birthdays = await JSON.parse(fs.readFileSync('./birthdays.json', 'utf8'));
+		birthdays.forEach((birthday: { day: number; month: number; user: User; }) => {
+			if (birthday.day == dateDay && birthday.month == dateMonth) {
+				client.channels.fetch(Keys.birthdayChannel).then(
+					channel => (channel as TextChannel).send(`FELIZ CUMPLE <@${birthday.user.id}>`)
+				);
+			}
+		});
+	},
+	start: true,
+	timeZone: 'Europe/Madrid'
+});
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,29 +1,6 @@
-import { CronJob } from 'cron';
 import { event, Events } from '../utils/index.js';
-import Keys from '../keys.js';
-import { TextChannel, User } from 'discord.js';
-import fs from 'fs';
 
 export default event(Events.ClientReady, async ({ log }, client) => {
-	var dateMonth = new Date().getMonth() + 1;
-	var dateDay = new Date().getDate();
-
-	const job = CronJob.from({
-		cronTime: `0 0 0 * * *`,
-		onTick: async function () {
-			var birthdays = await JSON.parse(fs.readFileSync('./birthdays.json', 'utf8'));
-			birthdays.forEach((birthday: { day: number; month: number; user: User; }) => {
-				if (birthday.day == dateDay && birthday.month == dateMonth) {
-					client.channels.fetch(Keys.birthdayChannel).then(
-						channel => (channel as TextChannel).send(`FELIZ CUMPLE <@${birthday.user.id}>`)
-					);
-				}
-			});			
-		},
-		start: true,
-		timeZone: 'Europe/Madrid'
-	});
-
 	return log(`Discord bot logged in as ${client.user.username}.`);
 })
 
